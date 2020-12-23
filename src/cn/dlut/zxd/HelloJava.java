727a6f6396ceae03d5156c4fe984a1fe1764341c
95138 <951383631@qq.com>
2020-12-23 09:12:39 +0800

    3.0

--- src/cn/dlut/zxd/HelloJava.java
+++ src/cn/dlut/zxd/HelloJava.java
@@ -4,5 +4,6 @@ public class HelloJava {
     public static void main(String[] args) {
         System.out.println("Hello Java");
         System.out.println("2.0");
+        System.out.println("3.0");
     }
 }
--- src/cn/dlut/zxd/HelloKotlin.kt
+++ src/cn/dlut/zxd/HelloKotlin.kt
@@ -3,4 +3,5 @@ package cn.dlut.zxd
 fun main() {
     println("Hello Kotlin")
     println("2.0000")
+    println("3.0")
 }
